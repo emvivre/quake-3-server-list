3e3c0051c95bb3289e6c5995535df0304f574542
emvivre <emvivre@safe-mail.net>
2016-07-16 01:39:26 +0200

    add server status request

--- q3_list.py
+++ q3_list.py
@@ -2,6 +2,7 @@
 
 import socket
 import struct
+import sys
 
 def get_ip_port(packet):
     p = packet[len('\xFF\xFF\xFF\xFFgetserversResponse'):]
@@ -23,10 +24,35 @@ MASTER = 'master.ioquake3.org'
 
 PORT = 27950
 
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-h':
+        print('Usage: %s [<SERVER>:<PORT>]' % sys.argv[0])
+        quit(1)    
+    (ip, port) = sys.argv[1].split(':')
+    port = int(port)
+    ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ss.settimeout(10)
+    ss.connect((ip, port))
+    ss.send(b'\xFF\xFF\xFF\xFFgetstatus')
+    dd = ss.recv(999999)
+    i = 0
+    status = {}
+    for e in dd.replace(b'\n',b'').split(b'\\')[1:]:
+        e = e.decode('utf-8')
+        if i % 2 == 0:
+            k = e
+        else:
+            status[k] = e
+        i += 1
+    for (k,v) in status.items():
+        print('  %s : %s' % (k, v))
+    ss.close()
+    quit(0)
+    
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(3)
 s.connect((MASTER, PORT))
-s.send(b"\xFF\xFF\xFF\xFFgetservers 68 empty full demo\n")
+s.send(b'\xFF\xFF\xFF\xFFgetservers 68 empty full demo')
 addr = {}
 d = s.recv(999999)
 for (ip, port) in get_ip_port(d):
